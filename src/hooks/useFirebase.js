35ffdd0e993d2178951870992fefafcceb4ad7bf
Zamal Hossain <thezamalian@gmail.com>
2022-02-13 00:22:08 +0600

    useFirebase hook is completed with all authentication system including google

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import initializeFirebase from '../Pages/Login/Firebase/firebase.init';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, updateProfile } from "firebase/auth";
 
 initializeFirebase();
 
@@ -8,10 +9,82 @@ const useFirebase = () => {
     const [authError, setAuthError] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    useEffect(() => {
+        setIsLoading(true);
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser({});
+            }
+            setIsLoading(false);
+        });
+        return unsubscribe;
+    }, [auth])
+
+    const registerWithEmail = (email, password, name, photo) => {
+        setIsLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setUser(userCredential.user);
+                const newUser = { ...user }
+                newUser.displayName = name;
+                newUser.photoURL = photo;
+                setUser(newUser);
+
+                updateProfile(auth.currentUser, {
+                    displayName: name, photoURL: photo
+                }).then(() => {
+                }).catch((error) => {
+                });
+            })
+            .catch((error) => {
+                setAuthError(error.message);
+            }).finally(() => setIsLoading(false));
+    }
+
+    const loginWithEmail = (email, password) => {
+        setIsLoading(true);
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setUser(userCredential.user);
+            })
+            .catch((error) => {
+                setAuthError(error.message);
+            }).finally(() => setIsLoading(false));
+    }
+
+    const googleSignIn = () => {
+        setIsLoading(true);
+        signInWithPopup(auth, googleProvider)
+            .then((userCredential) => {
+                setUser(userCredential.user);
+            })
+            .catch((error) => {
+                setAuthError(error.message);
+            }).finally(() => setIsLoading(false));
+    }
+
+    const logout = () => {
+        setIsLoading(true);
+        signOut(auth).then(() => {
+            setUser({});
+        }).catch((error) => {
+            setAuthError('');
+        }).finally(() => setIsLoading(false));
+    }
+
     return {
         user,
         authError,
-        isLoading
+        isLoading,
+        registerWithEmail,
+        loginWithEmail,
+        googleSignIn,
+        logout
     };
 };
 
